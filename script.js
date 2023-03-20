e1c827d00109020e5d77037d4242bb2ec7e7b0fa
victorbrambati <victorbrambati@gmail.com>
2023-03-20 19:00:04 -0300

    feat: shoot projectiles

--- script.js
+++ script.js
@@ -20,8 +20,54 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, '#f1c40f');
-player.draw();
+
+const projectiles = [];
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+  projectiles.forEach(projectile => {
+    projectile.update()
+  })
+}
+
+addEventListener('click', (event) => {
+    const angle = Math.atan2(event.clientY - y, event.clientX - x);
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+
+projectiles.push(
+    new Projectile(x, y, 5, 'red', velocity)
+)
+})
+
+animate();

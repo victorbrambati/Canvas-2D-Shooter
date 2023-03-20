3f0b80a6082855ef936b170a8296a8af047bacf9
victorbrambati <victorbrambati@gmail.com>
2023-03-20 18:41:00 -0300

    add player

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-console.log(canvas)
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, '#f1c40f');
+player.draw();
